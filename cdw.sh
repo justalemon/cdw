fc1457cb9ce94d8af6b1aa307ca06215149cb1c5
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-05 18:00:21 -0400

    Put all variables between double quotes

--- cdw.sh
+++ cdw.sh
@@ -11,16 +11,16 @@ fi
 # The raw representation of the launch parameters
 RAW="$*"
 # Cut the letter from the beginning to see if is absolute or relative
-LETTER=$(echo $RAW | cut -d ':' -f 1 -s | tr '[:upper:]' '[:lower:]')
+LETTER=$(echo "$RAW" | cut -d ':' -f 1 -s | tr '[:upper:]' '[:lower:]')
 # Replace back slashes with forward slashes
 # Then trim the letters from the beginning
 # Finally remove the slash at the beginning, if any
-UNIX=$(echo $RAW | sed -r 's@\\@/@g; s@^[A-Z]:@@; s@^/@@')
+UNIX=$(echo "$RAW" | sed -r 's@\\@/@g; s@^[A-Z]:@@; s@^/@@')
 
-if [ -z $LETTER ]; then
+if [ -z "$LETTER" ]; then
     cd "$UNIX"
 else
     cd "/mnt/$LETTER/$UNIX"
 fi
 
-exec $SHELL
+exec "$SHELL"
